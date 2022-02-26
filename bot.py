09a89998592c44edf3bcedaa66a3703eec92a194
dynamite14 <casgaming14@gmail.com>
2022-02-26 15:04:30 -0500

    fix

--- bot.py
+++ bot.py
@@ -8,8 +8,6 @@ from ext.owner import *
 from ext.test import *
 from ext.misc import *
 
-TOKEN = os.getenv("TOKEN")
-
 bot = commands.Bot(command_prefix="c!", description="CASbot is a test bot created by CAS, aka >>#0001.", owner_ids=u.owners)
 
 logging.basicConfig(level=logging.INFO)
@@ -33,7 +31,7 @@ async def on_message(message):
 if os.getlogin() == "casga":
     with open("C:\\Users\\casga\\Desktop\\bot-token.txt", "r") as f:
         TOKEN = f.read()
-        
+
 else:
     TOKEN = os.getenv("TOKEN")
 
